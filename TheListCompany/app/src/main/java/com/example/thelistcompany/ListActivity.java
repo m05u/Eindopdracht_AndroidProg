423ffae8a7af89df6dbb72082e4977c406a8c210
Moustapha Azaimi <m.azaimi@student.avans.nl>
2022-06-20 07:09:09 +0200

    datepickerdialog workey
    YEEYYYY

--- TheListCompany/app/src/main/java/com/example/thelistcompany/ListActivity.java
+++ TheListCompany/app/src/main/java/com/example/thelistcompany/ListActivity.java
@@ -1,24 +1,18 @@
 package com.example.thelistcompany;
 
-import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.DatePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.Objects;
-
-import Models.Launch;
 
 public class ListActivity extends AppCompatActivity
         implements LaunchesAdapter.OnItemClickListener {
@@ -27,6 +21,8 @@ public class ListActivity extends AppCompatActivity
     private LaunchesAdapter launchesAdapter;
     private Button startDateEntry;
     private Button endDateEntry;
+    private DatePickerDialog startDialog;
+    private DatePickerDialog endDialog;
 
 
     @Override
@@ -35,6 +31,10 @@ public class ListActivity extends AppCompatActivity
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_list);
 
+        startDateEntry = findViewById(R.id.startDateEntry);
+        endDateEntry = findViewById(R.id.endDateEntry);
+
+
         updateButton = (Button) findViewById(R.id.filterButton);
         launchesRv = (RecyclerView) findViewById(R.id.recyclerView);
 
@@ -42,38 +42,89 @@ public class ListActivity extends AppCompatActivity
         launchesRv.setAdapter(launchesAdapter);
         launchesRv.setLayoutManager(new LinearLayoutManager(this));
 
-        startDateEntry = findViewById(R.id.startDateEntry);
-        startDateEntry = findViewById(R.id.endDateEntry);
+        startDateEntry.setText(getTodaysDate());
+        endDateEntry.setText(getTodaysDate());
+        startDialog = initDatePicker(startDateEntry);
+        endDialog =  initDatePicker(endDateEntry);
 
+
+    }
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private DatePickerDialog initDatePicker(Button button) {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+//                startDateEntry.setText(date);
+                button.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+        return datePickerDialog;
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if (month == 1)
+            return "JAN";
+        if (month == 2)
+            return "FEB";
+        if (month == 3)
+            return "MAR";
+        if (month == 4)
+            return "APR";
+        if (month == 5)
+            return "MAY";
+        if (month == 6)
+            return "JUN";
+        if (month == 7)
+            return "JUL";
+        if (month == 8)
+            return "AUG";
+        if (month == 9)
+            return "SEP";
+        if (month == 10)
+            return "OCT";
+        if (month == 11)
+            return "NOV";
+        if (month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void openStartDatePicker(View view) {
+        startDialog.show();
+    }
+    public void openEndDatePicker(View view) {
+        endDialog.show();
     }
 
     @Override
     public void onItemClick(int clickPosition) {
-
-    }
-
-    public void filterLaunches() {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-
-        Date startDate = null;
-        Date endDate = null;
-        try {
-            String startString = (startDateEntry.getText().toString());
-            String endString = (endDateEntry.getText().toString());
-
-            startDate = sdf.parse(startString);
-
-
-            startDate = sdf.parse(startString);
-        } catch (java.text.ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            Log.i("E11111111111", e.toString());
-        }
-
-    }
-
-    public void openDatePicker(View view) {
 
     }
 }
